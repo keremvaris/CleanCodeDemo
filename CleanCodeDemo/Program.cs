b7ec4317f80f2ce75a7622380f16e88cfacd0df3
Kerem VARIŞ <webyonett@gmail.com>
2018-11-26 19:23:19 +0300

    code review

--- CleanCodeDemo/Program.cs
+++ CleanCodeDemo/Program.cs
@@ -8,8 +8,8 @@ namespace CleanCodeDemo
     {
       IProductService productService = new ProductManager(new IsBankServiceAdapter());
       productService.Sell(
-        new Product { ProductId = 1, ProductName = "Laptop", ProductPrice = 1000 }, 
-        new Student(){ Id = 1, Name = "Kerem" }
+        new Product { ProductId = 1, ProductName = "Laptop", ProductPrice = 1000 },
+        new Student { Id = 1, Name = "Kerem" }
         );
       Console.ReadLine();
     }
