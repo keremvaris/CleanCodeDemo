ef6648fe2991812d1d8580f751f5d3335a85ce69
Kerem VARIŞ <webyonett@gmail.com>
2018-11-28 08:11:58 +0300

    refactoring

--- CleanCodeDemo/Entities/Customer.cs
+++ CleanCodeDemo/Entities/Customer.cs
@@ -4,6 +4,10 @@ namespace CleanCodeDemo
 {
   public class Customer : IEntity, IPerson
   {
+    public Customer()
+    {
+      CampaignHandler = new CustomerCampaignHandler();
+    }
     public int Id { get; set; }
     public string Name { get; set; }
     public IPersonCampaignHandler CampaignHandler { get; set; }
--- CleanCodeDemo/Entities/Officer.cs
+++ CleanCodeDemo/Entities/Officer.cs
@@ -4,6 +4,10 @@ namespace CleanCodeDemo
 {
   public class Officer : IEntity, IPerson
   {
+    public Officer()
+    {
+      CampaignHandler = new OfficerCampaignHandler();
+    }
     public int Id { get; set; }
     public string Name { get; set; }
     public IPersonCampaignHandler CampaignHandler { get; set; }
